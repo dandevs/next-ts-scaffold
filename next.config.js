46cc4f7a77c3d263de2b252b35cc34263a3b9986
dandevs <danilodevs@gmail.com>
2018-06-11 12:23:33 -0400

    Added lodash webpack plugin

--- next.config.js
+++ next.config.js
@@ -1,1 +1,13 @@
-module.exports = require("@zeit/next-typescript")();
+const withTypescript = require("@zeit/next-typescript");
+const lodashPlugin = require("lodash-webpack-plugin");
+
+module.exports = withTypescript({
+    webpack: (config) => {
+        config.plugins.push(new lodashPlugin({
+            currying:     true,
+            placeholders: true,
+        }));
+
+        return config;
+    },
+});
